f3881cedac1acf25657cf5cf4460c051e83067ba
Marek15 <marekhth@gmail.com>
2021-05-16 14:02:48 +0200

    created Excersise model migration and controller

---
+++ database/migrations/2021_05_16_113808_create_exercises_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExercisesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exercises', function (Blueprint $table) {
+            $table->id();
+            $table->string('url')->unique();
+            $table->string('name')->unique();
+
+            $table->foreignId('body_part_id')->constrained('body_parts');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exercises');
+    }
+}
