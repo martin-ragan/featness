ec21775ac8200bbf8a46b059a288cc6624c6513c
Marek15 <marekhth@gmail.com>
2021-05-16 14:56:24 +0200

    added some default inserts to exercises table

--- database/migrations/2021_05_16_113808_create_exercises_table.php
+++ database/migrations/2021_05_16_113808_create_exercises_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class CreateExercisesTable extends Migration
 {
@@ -21,6 +22,26 @@ class CreateExercisesTable extends Migration
             $table->foreignId('body_part_id')->constrained('body_parts');
 
         });
+
+        DB::table('exercises')->insert([
+            [
+                'name' => 'drep',
+                'url' => '549607068',
+                'body_part_id' => 2,
+            ],
+            [
+                'name' => 'drep s výskokom',
+                'url' => '549607347',
+                'body_part_id' => 2,
+            ],
+            [
+                'name' => 'drep s výskokom na stranu',
+                'url' => '549607620',
+                'body_part_id' => 2,
+            ],
+        ]);
+
+
     }
 
     /**
